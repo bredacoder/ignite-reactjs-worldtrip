f1bf00009773f638f04bc41028c1b67b145f78eb
Daniel Silva <daniel.silva@semantix.ai>
2022-11-29 12:18:57 -0400

    Travel Types and Responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ export function Header() {
       as="header"
       w="100%"
       maxW="1160"
-      h="20"
+      h={["10", "20"]}
       justify="center"
       align="center"
       mx="auto"
     >
-      <Image src="Logo.svg" alt="Logo" h="10" />
+      <Image src="Logo.svg" alt="Logo" h={["5", "10"]} />
     </Flex>
   )
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+
+const TravelTypes = [
+  {
+    image: 'TravelTypes/Cocktail.svg',
+    description: 'vida noturna'
+  },
+  {
+    image: 'TravelTypes/Surf.svg',
+    description: 'praia'
+  },
+  {
+    image: 'TravelTypes/Building.svg',
+    description: 'moderno'
+  },
+  {
+    image: 'TravelTypes/Museum.svg',
+    description: 'clássico'
+  },
+  {
+    image: 'TravelTypes/Earth.svg',
+    description: 'e mais...'
+  },
+];
 
 export default function Home() {
   return (
@@ -11,7 +34,7 @@ export default function Home() {
         bgPosition="center"
         bgRepeat="no-repeat"
         bgSize="cover"
-        h="335"
+        h={["163", "335"]}
         w="100vw"
       >
         <Flex 
@@ -21,17 +44,18 @@ export default function Home() {
           mx="auto"
           h="100%"
           maxW="1160"
+          px={["4", "6"]}
         >
           <Box>
             <Text 
-              fontSize="4xl" 
+              fontSize={["xl", "4xl"]}
               fontWeight="500" 
               color="gray.100"
             >
               5 Continentes,
             </Text>
             <Text
-              fontSize="4xl"
+              fontSize={["xl", "4xl"]}
               fontWeight="500"
               color="gray.100"
             >
@@ -39,10 +63,14 @@ export default function Home() {
             </Text>
       
             <Text
-              fontSize="xl"
+              fontSize={{
+                base: "sm",
+                sm: "xl",
+              }}
               fontWeight="400"
               color="gray.200"
-              mt="5" maxW="32.75rem"
+              mt="5" 
+              maxW="32.75rem"
             >
               Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
             </Text>
@@ -51,15 +79,56 @@ export default function Home() {
           <Image
             position="absolute"
             right="0"
-            bottom="-10"
+            bottom={{
+              base: "60%",
+              sm: "-11",
+              md: "-12",
+              lg: "-10"
+            }}
             zIndex="1"
-            h="300"
+            h={{
+              base: "60px",
+              sm: "120px",
+              md: "240px",
+              lg: "300px"
+            }}
             src="Airplane.svg"
             alt="Airplane"
           />
        
         </Flex>
       </Box>
+
+      <Flex
+        w="100%"
+        maxW="1160"
+        mx="auto"
+      >
+        <SimpleGrid
+          mt={["9", "14", "28"]}
+          minChildWidth={["170px", "200px"]}
+          spacing={["4", "6"]} 
+          w="100%"
+        >
+          {TravelTypes.map(travelType => (
+            <Flex direction="column" align="center" gap={["2", "4", "6"]}>
+              <Image
+                width={["30px", "60px", "85px"]}
+                height={["30px", "60px", "85px"]}
+                src={travelType.image}
+                alt={travelType.description}
+              />
+              <Text
+                fontWeight="600"
+                fontSize={["lg", "xl", "2xl"]}
+              >
+                {travelType.description}
+              </Text>
+            </Flex>
+          ))}
+        </SimpleGrid>
+        
+      </Flex>
     </>
   )
 }
