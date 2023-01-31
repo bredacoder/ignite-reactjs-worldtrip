cb3290e53b86db9e613dcf39d09e763f22dc081a
Daniel Felipe Breda da Silva <daniefelipedeveloper@gmail.com>
2023-01-30 22:04:52 -0400

    feat: Info Icon

--- src/components/ContinentContent/Infos.tsx
+++ src/components/ContinentContent/Infos.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 export default function Infos() {
   return (
@@ -52,12 +52,21 @@ export default function Infos() {
           27
         </Heading>
         <Text
+          position="relative"
           fontWeight="600"
           fontSize="2xl"
           lineHeight="9"
           color="gray.600"
         >
           cidades +100
+
+          <Image
+            position="absolute"
+            top="50%"
+            transform="translateY(-50%)"
+            right="-30px"
+            src="/Info.svg"
+          />
         </Text>
       </Box>
   </Flex>
