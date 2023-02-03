5ba31ca7619ac39ce9780192ca2f816dd701997b
Daniel Felipe Breda da Silva <daniefelipedeveloper@gmail.com>
2023-02-03 09:33:23 -0400

    feat: Header Navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,37 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Grid, Icon, Image } from "@chakra-ui/react";
+import { RiArrowLeftSLine } from 'react-icons/ri';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const { asPath } = useRouter();
+  const notHomePage = asPath !== '/';
+
   return (
-    <Flex
-      as="header"
-      w="100%"
-      maxW="1160"
-      h={["10", "20"]}
-      justify="center"
-      align="center"
-      mx="auto"
-    >
-      <Image src="/Logo.svg" alt="Logo" h={["5", "10"]} />
+    <Flex bg="white" w="100%" as="header" mx="auto" px="1rem" h={["50px","100px"]} align="center" justify="center">
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+        alignSelf="start"
+      >
+        {notHomePage && (
+          <Link href="/">
+            <Icon as={RiArrowLeftSLine} fontSize={[20,40]} justifySelf="start"/>
+          </Link>
+        )}
+          <Image
+            w={["81px","184px"]}
+            src="/Logo.svg"
+            alt="Logo"
+            justifySelf="center"
+            gridColumn="2"
+          />
+      </Grid>
     </Flex>
   )
 };
